c7bcb23c99590f6e1dad17a4355976a5d91b6621
MahdiTalebii <92058431+MahdiTalebii@users.noreply.github.com>
2022-11-17 22:12:54 +0330

    Add files via upload

---
+++ program5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program5
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            SQRT(n);
+        }
+
+        private static void SQRT(int n)
+        {
+            Console.WriteLine(Math.Sqrt(n));
+        }
+    }//endclass
+}
